604bc9777ee0eef8af1afaeceb96ae9fb3205521
Khushal_Agarwal <starkhush5@gmail.com>
2022-03-12 23:55:41 +0530

    Initial commit

---
+++ CP/Codeforces/word_capitalization.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    string s;
+    cin >> s;
+    s[0] = toupper(s[0]);
+    cout << s;
+    return 0;
+}
