d38e570b90782173918ceb7618c2e47ea0cfa1be
Khushal_Agarwal <starkhush5@gmail.com>
2022-04-02 00:29:54 +0530

    question solved successfully

--- CP/Codeforces/panoramix_prediction.cpp
+++ CP/Codeforces/panoramix_prediction.cpp
@@ -8,27 +8,14 @@ int main()
 {
 	ios::sync_with_stdio(0);
 
-	int n, m;
-	cin >> n >> m;
-	int temp;
-	while (1)
+	int t;
+	cin >> t;
+	while (t--)
 	{
-		int f = 0;
-		int i = ++n;
-		for (int j = 2; j <= sqrt(i); j++)
-		{
-			if (i % j == 0) {
-				f = 1;
-				break;
-			}
-		}
-		if (f == 0) {
-			temp = i;
-			break;
-		}
+		int n;
+		cin >> n;
+
 	}
-	if (temp == m) cout << "YES";
-	else cout << "NO";
 
 	return 0;
 }
