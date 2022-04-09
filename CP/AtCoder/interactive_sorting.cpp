d1b1eeec6b5c84d2a164061eeebdd5d22a5468fb
Khushal_Agarwal <starkhush5@gmail.com>
2022-04-09 22:50:27 +0530

    initial commit

---
+++ CP/AtCoder/interactive_sorting.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define f(n) for(int i=0;i<n;i++)
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+
+	}
+	return 0;
+}
---
+++ CP/AtCoder/welcome.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define f(n) for(int i=0;i<n;i++)
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+
+	int a, b, c;
+	cin >> a >> b >> c;
+	string s;
+	cin >> s;
+	cout << a + b + c << " " << s;
+
+	return 0;
+}
