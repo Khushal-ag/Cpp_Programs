0ec823416868189378e9a2059a1ff8c635f0fdb7
Khushal_Agarwal <starkhush5@gmail.com>
2022-04-09 22:53:52 +0530

    initial commit

---
+++ CP/Codeforces/elephant.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define f(n) for(int i=0;i<n;i++)
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+
+	int t;
+	cin >> t;
+	cout << ceil(t / 5.0);
+	return 0;
+}
