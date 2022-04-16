9b39f4c8d90a1cddd1ddd89be2042625b11a734a
Khushal_Agarwal <starkhush5@gmail.com>
2022-04-16 23:36:34 +0530

    Initial commit

---
+++ CP/Codeforces/seerja_and_deema.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define f(n) for(int i=0;i<n;i++)
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+
+	int n;
+	cin >> n;
+	vector<int> v;
+	for (int i = 0; i < n; i++)
+		cin >> v[i];
+	sort(v.begin(), v.end());
+	int max = 0, min = 0;
+	for (int i = 0; i < v.size(); i++)
+		if (i % 2 == 0) max += v[i];
+		else min += v[i];
+	cout << max << " " << min;
+
+
+	return 0;
+}
