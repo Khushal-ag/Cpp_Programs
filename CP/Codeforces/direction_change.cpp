8db2ef017fcf7f3720ae4ad7d4986d02c8909e5b
Khushal_Agarwal <starkhush5@gmail.com>
2022-04-21 23:14:30 +0530

    initial commit

---
+++ CP/Codeforces/direction_change.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define f(n) for(int i=0;i<n;i++)
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n, m;
+		cin >> n >> m;
+
+	}
+
+	return 0;
+}
