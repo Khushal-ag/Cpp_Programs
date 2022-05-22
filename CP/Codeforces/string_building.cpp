a7ff10419523445a3b1217aa5d4d0153cd94a2e4
Khushal_Agarwal <starkhush5@gmail.com>
2022-05-22 16:30:52 +0530

    initial commit

---
+++ CP/Codeforces/string_building.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define f(n) for(int i=0;i<n;i++)
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		string s;
+		cin >> s;
+		int a_count = 0, b_count = 0, f = 0;
+		for (auto i : s)
+		{
+			if (i == 'a') {
+				a_count++;
+			}
+		}
+	}
+	return 0;
+}
