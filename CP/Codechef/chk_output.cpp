86feb425d62cc6f6d812370bf4c5ea43c2f88fac
Khushal-ag <starkhush5@gmail.com>
2023-04-13 22:38:59 +0530

    cp codes

--- CP/Codechef/chk_output.cpp
+++ CP/Codechef/chk_output.cpp
@@ -1,24 +1,37 @@
+/*
+	Author : Khushal_Agarwal
+*/
+
 #include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long int
+#define lld long double
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define p(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define s(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define sf(x) scanf("%lf", &x)
+#define INF 1e18+9
+#define endl '\n'
+#define mod 1000000007
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
 
 using namespace std;
-bool isprime(int s)
-{
-	for(int i=2;i<=sqrt(s);i++)
-			if(s%i==0) return false;
-		return true;
-}
-int main(){
-	
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		int a,b;
-		cin>>a>>b;
-		int s = a+b;
-		if(isprime(s)) cout<<"Alice\n";
-		else cout<<"Bob\n";
 
-	}
-	return 0;
+int main(){
+	ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+
+	int t;
+    cin>>t;
+    while(t--){
+    	
+
+    }
+    return 0;					
 }
